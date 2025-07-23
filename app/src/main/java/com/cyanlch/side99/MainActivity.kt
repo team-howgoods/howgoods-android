5844fc3671cfabc1963eae91dc0a4511564bee10
cyanO94 <dhdnjscks@gmail.com>
2025-07-23 23:43:11 +0900

    refactor: SplashActivity.kt to RoutingActivity.kt

--- app/src/main/java/com/cyanlch/side99/MainActivity.kt
+++ app/src/main/java/com/cyanlch/side99/MainActivity.kt
@@ -2,12 +2,9 @@ package com.cyanlch.side99
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
-class SplashActivity: ComponentActivity() {
-
+class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
     }
 }
---
+++ app/src/main/java/com/cyanlch/side99/RoutingActivity.kt
@@ -0,0 +1,17 @@
+package com.cyanlch.side99
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class RoutingActivity: ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+    }
+}
