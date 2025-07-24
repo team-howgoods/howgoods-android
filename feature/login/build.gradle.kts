1608a64117952045c7bb0d5f78b04cc393a9137c
cyanO94 <dhdnjscks@gmail.com>
2025-07-24 21:19:41 +0900

    add: Apple Login Screen

--- feature/login/build.gradle.kts
+++ feature/login/build.gradle.kts
@@ -17,4 +17,12 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.browser)
+
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.foundation)
+
 }
