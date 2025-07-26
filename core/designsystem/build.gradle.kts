a0dceb778014d35a7be7b6649db648ca842402d5
cyanO94 <dhdnjscks@gmail.com>
2025-07-27 00:52:08 +0900

    update: ui, design system

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -1,43 +1,13 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.cyanlch.android.library.compose)
 }
 
 android {
     namespace = "com.cyanlch.designsystem"
-    compileSdk = 35
-
-    defaultConfig {
-        minSdk = 31
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    api(libs.androidx.compose.foundation)
+    api(libs.androidx.compose.ui)
+    api(libs.androidx.compose.material3)
 }
---
+++ core/ui/src/main/java/com/cyanlch/ui/SocialButtons.kt
@@ -0,0 +1,57 @@
+package com.cyanlch.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.unit.dp
+import com.cyanlch.designsystem.CyanOutlinedButton
+import com.cyanlch.designsystem.WidthSpacer
+
+
+@Composable
+fun SocialButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    isEnabled: Boolean,
+    imagePainter: Painter,
+    textColor: Color,
+    backgroundColor: Color,
+    onClick: () -> Unit,
+) {
+    CyanOutlinedButton(
+        enabled = isEnabled,
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = backgroundColor,
+            contentColor = textColor,
+        ),
+        content = @Composable {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center,
+                modifier = Modifier.padding(vertical = 16.dp),
+            ) {
+                Image(
+                    painter = imagePainter,
+                    contentDescription = "Button Icon",
+                )
+                WidthSpacer(16)
+
+                Text(
+                    text = text,
+                )
+            }
+        },
+        modifier = modifier
+            .fillMaxWidth()
+    )
+}
