6ab3c3770fea0d8d66aa8e59df7547b2c035b3bb
cyanO94 <dhdnjscks@gmail.com>
2025-07-27 01:02:58 +0900

    add: apple login screen navigation

--- feature/login/src/main/java/com/cyanlch/login/LoginScreen.kt
+++ feature/login/src/main/java/com/cyanlch/login/LoginScreen.kt
@@ -1,23 +1,22 @@
 package com.cyanlch.login
 
+import android.content.Intent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import com.cyanlch.ui.SocialButton
-import com.cyanlch.ui.circuit.NoState
 import com.cyanlch.ui.R
 import com.slack.circuit.codegen.annotations.CircuitInject
+import com.slack.circuit.runtime.CircuitUiEvent
 import com.slack.circuit.runtime.CircuitUiState
-import com.slack.circuit.runtime.Navigator
 import com.slack.circuit.runtime.presenter.Presenter
 import com.slack.circuit.runtime.screen.Screen
-import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import dagger.hilt.android.components.ActivityRetainedComponent
@@ -25,13 +24,26 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class LoginScreen(val nextScreen: Screen? = null) : Screen {
-    data object State : CircuitUiState
+    data class State(
+        val eventSink: (Event) -> Unit
+    ): CircuitUiState
+
+    sealed interface Event : CircuitUiEvent {
+        data object OpenAppleLogin : Event
+    }
 }
 
 class LoginPresenter @AssistedInject constructor() : Presenter<LoginScreen.State> {
     @Composable
     override fun present(): LoginScreen.State {
-        return LoginScreen.State
+        val context = LocalContext.current
+        return LoginScreen.State { event ->
+            when (event) {
+                LoginScreen.Event.OpenAppleLogin -> context.startActivity(
+                    Intent(context, AppleLoginWithCustomTabsActivity::class.java)
+                )
+            }
+        }
     }
 
     @CircuitInject(LoginScreen::class, ActivityRetainedComponent::class)
@@ -56,7 +68,7 @@ fun LoginUi(state: LoginScreen.State, modifier: Modifier) {
             imagePainter = painterResource(id = R.drawable.ic_apple),
             textColor = Color.Black,
             backgroundColor = Color.White,
-            onClick = {}
+            onClick = { state.eventSink(LoginScreen.Event.OpenAppleLogin) }
         )
     }
 }
