a1c258e8b996a710703cb406e06ccc83e08c91e6
cyanO94 <dhdnjscks@gmail.com>
2025-07-27 21:04:24 +0900

    add: core-navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,7 @@ android {
 dependencies {
     implementation(projects.core.ui)
     implementation(projects.core.designsystem)
+    implementation(projects.core.navigation)
     implementation(projects.feature.login)
 
     implementation(libs.splashscreen)
---
+++ core/navigation/build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    alias(libs.plugins.cyanlch.android.library.compose)
+}
+
+android {
+    namespace = "com.cyanlch.navigation"
+}
+
+dependencies {
+}
