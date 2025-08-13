525c57d6a6d28c98f29ed5e80945dc3148d23b70
cyanO94 <dhdnjscks@gmail.com>
2025-08-13 21:50:49 +0900

    add: data store module

---
+++ core/datastore/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    alias(libs.plugins.cyanlch.android.library)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+android {
+    namespace = "com.cyanlch.datastore"
+}
+
+dependencies {
+    api(projects.core.data)
+
+}
