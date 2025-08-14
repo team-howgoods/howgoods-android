02527d0f9dd8e1c55883318f7567c09f9b75804f
cyanO94 <dhdnjscks@gmail.com>
2025-08-14 21:53:00 +0900

    feat: GetUserLoginStateUseCase

---
+++ core/domain/src/main/java/com/cyanlch/domain/usecase/auth/GetUserLoginStateUseCase.kt
@@ -0,0 +1,30 @@
+package com.cyanlch.domain.usecase.auth
+
+import com.cyanlch.domain.model.auth.UserToken
+import com.cyanlch.domain.repository.AuthRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+sealed interface LoginState {
+    data object UnAuthorized : LoginState
+    data object NotLogin : LoginState
+    data class Login(val userToken: UserToken) : LoginState
+}
+
+class GetUserLoginStateUseCase @Inject constructor(
+    private val authRepository: AuthRepository
+) {
+    suspend operator fun invoke(): LoginState {
+        return withContext(Dispatchers.IO) {
+            val userInfo = authRepository.getUserTokenFlow().firstOrNull()
+            if (userInfo == null) {
+                LoginState.NotLogin
+            } else {
+                LoginState.Login(userInfo)
+            }
+        }
+    }
+}
