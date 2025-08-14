b7f771895c2525c22d455d094d688380abbf42a0
cyanO94 <dhdnjscks@gmail.com>
2025-08-14 21:20:53 +0900

    refactor: DataStoreModule.kt

--- core/datastore/src/main/java/com/cyanlch/datastore/di/DataStoreModule.kt
+++ core/datastore/src/main/java/com/cyanlch/datastore/di/DataStoreModule.kt
@@ -2,8 +2,11 @@ package com.cyanlch.datastore.di
 
 import android.content.Context
 import androidx.datastore.core.DataStore
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.preferencesDataStore
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.preferencesDataStoreFile
 import com.cyanlch.data.datasource.auth.UserTokenDataStore
 import com.cyanlch.datastore.UserTokenDataStoreImpl
 import dagger.Binds
@@ -12,28 +15,34 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object DataStoreModule {
-
-    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "user_preferences")
-
-    @Provides
-    @Singleton
-    fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
-        return context.dataStore
-    }
-}
-
-@Module
-@InstallIn(SingletonComponent::class)
-interface DataSourceModule {
+abstract class DataStoreModule {
 
     @Binds
     @Singleton
-    fun bindUserTokenDataStore(
+    abstract fun bindUserTokenDataStore(
         userTokenDataStoreImpl: UserTokenDataStoreImpl
     ): UserTokenDataStore
+
+    companion object {
+        @Provides
+        @Singleton
+        fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+            return PreferenceDataStoreFactory.create(
+                corruptionHandler = ReplaceFileCorruptionHandler {
+                    it.printStackTrace()
+                    emptyPreferences()
+                },
+                migrations = emptyList(),
+                produceFile = { context.preferencesDataStoreFile("user_preferences") },
+                scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+            )
+        }
+    }
 }
