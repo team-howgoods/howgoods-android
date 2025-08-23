013e1122adb41fe798f0feb1b390aee54ce6facd
cyanO94 <dhdnjscks@gmail.com>
2025-08-23 16:04:54 +0900

    add: BookmarkIcon
    Change-Id: I414e974cea4c023f29e1fb45997db11bf1685f47

---
+++ core/designsystem/src/main/java/com/cyanlch/designsystem/icons/BookmarkIcon.kt
@@ -0,0 +1,59 @@
+package com.cyanlch.designsystem.icons
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.cyanlch.designsystem.R
+import com.cyanlch.designsystem.ui.HGTheme
+
+@Composable
+fun BookmarkBadgeIcon(
+    modifier: Modifier = Modifier,
+    size: Int = 24,
+    checked: Boolean,
+) {
+    val c = HGTheme.colors
+    val x = HGTheme.extras
+
+    val fillTint = when {
+        checked  -> c.primary
+        else     -> x.lineDefault
+    }
+
+    Box(modifier.size(size.dp)) {
+        Icon(
+            painter = painterResource(R.drawable.ic_bookmark_fill_24),
+            contentDescription = null,
+            tint = fillTint,
+            modifier = Modifier.matchParentSize()
+        )
+        Icon(
+            painter = painterResource(R.drawable.ic_bookmark_stroke_24),
+            contentDescription = null,
+            tint = Color.Unspecified,
+            modifier = Modifier.matchParentSize(),
+        )
+    }
+}
+
+@Preview
+@Composable
+fun BookmarkIconPreview() {
+    Column {
+        HGTheme(darkTheme = false) {
+            BookmarkBadgeIcon(checked = false)
+            BookmarkBadgeIcon(checked = true)
+        }
+        HGTheme(darkTheme = true) {
+            BookmarkBadgeIcon(checked = false)
+            BookmarkBadgeIcon(checked = true)
+        }
+    }
+}
