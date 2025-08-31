ebb7b73c92ed1dfd7115d9a62469b5711bfd059e
cyanO94 <dhdnjscks@gmail.com>
2025-08-31 22:26:22 +0900

    feat: remove gradiant
    Change-Id: If09809a988251ab01fdd113608d52ecba1ab0d88

--- feature/survey/src/main/java/com/cyanlch/survey/component/SurveyBottomBar.kt
+++ feature/survey/src/main/java/com/cyanlch/survey/component/SurveyBottomBar.kt
@@ -5,15 +5,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
-import com.cyanlch.designsystem.decoration.WhiteFadeBar
 import com.cyanlch.designsystem.ui.LocalHGColors
 
 @Composable
@@ -24,19 +19,10 @@ internal fun SurveyBottomBar(
         modifier = Modifier
             .background(LocalHGColors.current.bgDefault)
             .fillMaxWidth()
-            .padding(horizontal = 16.dp)
-            .padding(bottom = 16.dp),
+            .padding(horizontal = 16.dp, vertical = 12.dp),
     ) {
         Row {
             buttons()
         }
-        WhiteFadeBar(
-            horizontal = false,
-            modifier = Modifier
-                .align(Alignment.TopCenter)
-                .height(24.dp)
-                .offset(y = (-24).dp)
-                .zIndex(1f),
-        )
     }
 }
