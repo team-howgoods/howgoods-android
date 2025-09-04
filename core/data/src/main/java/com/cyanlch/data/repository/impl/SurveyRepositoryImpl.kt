cdaf795d628c4cf5447087da79a39300364ebf3d
somin <salth0601@gmail.com>
2025-09-04 17:20:02 +0900

    feat(data): repository에 애니메이션 리스트를 불러오는 api 연결

--- core/data/src/main/java/com/cyanlch/data/repository/impl/SurveyRepositoryImpl.kt
+++ core/data/src/main/java/com/cyanlch/data/repository/impl/SurveyRepositoryImpl.kt
@@ -1,14 +1,17 @@
 package com.cyanlch.data.repository.impl
 
+import com.cyanlch.data.datasource.survey.SurveyDataSource
 import com.cyanlch.domain.model.anime.Anime
 import com.cyanlch.domain.model.anime.AnimeCharacter
 import com.cyanlch.domain.model.anime.AnimeId
 import com.cyanlch.domain.repository.SurveyRepository
 import javax.inject.Inject
 
-class SurveyRepositoryImpl @Inject constructor() : SurveyRepository {
+class SurveyRepositoryImpl @Inject constructor(
+    private val dataSource: SurveyDataSource,
+) : SurveyRepository {
     override suspend fun fetchAnimeCatalog(): List<Anime> {
-        return emptyList()
+        return dataSource.fetchAnimeCatalog()
     }
 
     override suspend fun fetchCharactersByAnime(animeId: AnimeId): List<AnimeCharacter> {
