a2ce66f7151e67ac8de51a53680fab6b2c1e5dd2
cyanO94 <dhdnjscks@gmail.com>
2025-09-06 19:20:39 +0900

    fix: 선택된 굿즈 caption 추가
    Change-Id: I75ac7321d15f437fdaec7c5970c76fd8c09bf93c

--- feature/survey/src/main/java/com/cyanlch/survey/selection/component/SelectedGoodsRow.kt
+++ feature/survey/src/main/java/com/cyanlch/survey/selection/component/SelectedGoodsRow.kt
@@ -11,16 +11,22 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.cyanlch.designsystem.HeightSpacer
 import com.cyanlch.designsystem.R
 import com.cyanlch.designsystem.WidthSpacer
 import com.cyanlch.designsystem.ui.HGColors
+import com.cyanlch.designsystem.ui.HGTypography
 import com.cyanlch.survey.model.SelectedGoods
 import com.cyanlch.survey.selection.GoodsSelectionScreen
 
@@ -48,11 +54,41 @@ private fun SelectedGoodsItem(
 ) {
     Box(
         modifier = modifier
-            .border(width = 1.dp, color = HGColors.iconDark, shape = RoundedCornerShape(8.dp)),
+            .clip(RoundedCornerShape(8.dp))
+            .border(
+                width = 1.dp,
+                color = HGColors.iconDark,
+                shape = RoundedCornerShape(8.dp),
+            ),
     ) {
         AsyncImage(
             model = goods.imageUrl,
             contentDescription = goods.name,
+        )
+
+        Box(
+            modifier = Modifier
+                .matchParentSize()
+                .background(
+                    Brush.verticalGradient(
+                        colorStops = arrayOf(
+                            0.0f to Color.Transparent,
+                            0.5f to Color.Transparent,
+                            1.0f to Color.Black,
+                        ),
+                    ),
+                ),
+        )
+        Text(
+            text = goods.name,
+            color = Color.White,
+            style = HGTypography.caption11SemiBold,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 6.dp)
+                .padding(horizontal = 6.dp),
         )
 
         Box(
