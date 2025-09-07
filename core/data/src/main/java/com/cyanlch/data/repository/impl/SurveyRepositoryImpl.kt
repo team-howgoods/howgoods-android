a092ef80a7718121161cdf0f2dde473e29a49872
somin <salth0601@gmail.com>
2025-09-07 15:18:05 +0900

    feat(data): repository에 캐릭터 리스트 불러오는 api 연결

--- core/data/src/main/java/com/cyanlch/data/repository/impl/SurveyRepositoryImpl.kt
+++ core/data/src/main/java/com/cyanlch/data/repository/impl/SurveyRepositoryImpl.kt
@@ -2,8 +2,7 @@ package com.cyanlch.data.repository.impl
 
 import com.cyanlch.data.datasource.survey.SurveyDataSource
 import com.cyanlch.domain.model.anime.Anime
-import com.cyanlch.domain.model.anime.AnimeCharacter
-import com.cyanlch.domain.model.anime.AnimeId
+import com.cyanlch.domain.model.character.AnimeCharacters
 import com.cyanlch.domain.repository.SurveyRepository
 import javax.inject.Inject
 
@@ -14,7 +13,7 @@ class SurveyRepositoryImpl @Inject constructor(
         return dataSource.fetchAnimeCatalog()
     }
 
-    override suspend fun fetchCharactersByAnime(animeId: AnimeId): List<AnimeCharacter> {
-        return emptyList()
+    override suspend fun fetchCharactersByAnime(animationIds: List<Int>): List<AnimeCharacters> {
+        return dataSource.fetchCharacters(animationIds)
     }
 }
--- core/domain/src/main/java/com/cyanlch/domain/repository/SurveyRepository.kt
+++ core/domain/src/main/java/com/cyanlch/domain/repository/SurveyRepository.kt
@@ -1,11 +1,10 @@
 package com.cyanlch.domain.repository
 
 import com.cyanlch.domain.model.anime.Anime
-import com.cyanlch.domain.model.anime.AnimeCharacter
-import com.cyanlch.domain.model.anime.AnimeId
+import com.cyanlch.domain.model.character.AnimeCharacters
 
 interface SurveyRepository {
     suspend fun fetchAnimeCatalog(): List<Anime>
-    suspend fun fetchCharactersByAnime(animeId: AnimeId): List<AnimeCharacter>
+    suspend fun fetchCharactersByAnime(animationIds: List<Int>): List<AnimeCharacters>
     // suspend fun submit(payload: SurveySubmitDto)
 }
